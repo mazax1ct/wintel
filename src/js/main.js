ce219aa169750e7218c9693f4f55ebb886f8710d
mazax1ct <vova@giveandget.ru>
2023-05-17 20:48:51 +0300

    all

--- src/js/main.js
+++ src/js/main.js
@@ -26,3 +26,48 @@ $(document).on('click', '.js-dropdown-closer', function () {
   $(this).closest('.dropdown').removeClass('is-open');
   return false;
 });
+
+
+//открытие попапа
+$(document).on('click', '.js-popup-opener', function () {
+  var popup = $(this).attr('data-popup');
+
+  $('body').addClass('popup-is-open');
+
+  $('.popups-container').fadeIn(300);
+
+  $('.popup-shade[data-popup="'+popup+'"]').fadeIn(300, function () {
+    $(this).find('.popup').addClass('is-open');
+  }).css('display', 'flex');
+
+  return false;
+});
+
+//закрытие попапа
+$(document).on('click', '.js-popup-closer', function () {
+  $(this).closest('.popup').removeClass('is-open');
+
+  $(this).closest('.popup-shade').fadeOut(300, function () {
+    if(!$('.popup-shade').is(':visible')) {
+      $('.popups-container').fadeOut(300);
+    }
+  });
+
+  $('body').removeClass('popup-is-open');
+
+  return false;
+});
+
+$(document).on('click', '.popup-shade', function (evt) {
+  if (!$('.popup').is(evt.target) && $('.popup').has(evt.target).length === 0) {
+    $(this).find('.popup').removeClass('is-open');
+
+    $(this).closest('.popup-shade').fadeOut(300, function () {
+      if(!$('.popup-shade').is(':visible')) {
+        $('.popups-container').fadeOut(300);
+      }
+    });
+
+    $('body').removeClass('popup-is-open');
+  }
+});
